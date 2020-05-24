e0de2f36067ca076a0af5b6a411d430ee01462c5
Memory_Leak <irealing@163.com>
2020-05-24 22:33:27 +0800

    feat: 更新版本号，更新读取文件操作

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 __author__ = 'Memory_Leak<irealing@163.com>'
-with open('README.md', 'r') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 setup(
     name='red_helper',
     packages=find_packages(),
     author="Memory_Leak",
-    version="0.0.1",
+    version="0.1.1",
     auth_email="irealing@163.com",
     python_requires=">=3.8",
     description="基于异步IO的Redis缓存操作工具",
